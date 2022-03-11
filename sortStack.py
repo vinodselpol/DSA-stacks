38f33fe904add73a168ea82e1c7244a151d27ac6
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2022-03-11 08:20:34 +0100

    Create sortStack.py

---
+++ sortStack.py
@@ -0,0 +1,17 @@
+def sortStack(stack):
+    if len(stack)==0:
+		return stack
+	top=stack.pop()
+	sortStack(stack)
+	insertInSortedOrder(stack, top)
+	return stack
+
+def insertInSortedOrder(stack, value):
+	if len(stack)==0 or stack[len(stack)-1] <=value:
+		stack.append(value)
+		return
+	
+	top=stack.pop()
+	insertInSortedOrder(stack, value)
+	
+	stack.append(top)
